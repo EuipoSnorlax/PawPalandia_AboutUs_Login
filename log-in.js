3b988464fae2120719268bab98a588ad850f8f6b
Romina <cv6494@innovaccion.mx>
2024-11-17 03:31:31 -0600

    Changes Login Form

--- log-in.js
+++ log-in.js
@@ -3,19 +3,42 @@ loginForm.addEventListener('submit', (e) => {
 
 e.preventDefault();
 
+const loginName = document.querySelector('#loginName').value;
 const loginEmail = document.querySelector('#loginEmail').value;
 const loginPassword = document.querySelector('#loginPassword').value;
+const container = document.querySelector('#alertContainer');
 
 const users = JSON.parse(localStorage.getItem('users')) || [];
 
-const userRegister = users.find(user => user.email === loginEmail);
-if (userRegister) {
-return alert("El usuario ya está registrado");
+const user = users.find(user => user.email === loginEmail);
+
+if (user) {
+
+    const alertHTML = ` <div class="alert alert-success alert-dismissible fade show" role="alert">
+        ¡Bienvenido!
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
+`;
+container.innerHTML = alertHTML;
+return;
 }
 
-users.push({ email: loginEmail, password: loginPassword });
-localStorage.setItem('users', JSON.stringify(users));
+users.push({ name: loginName, email: loginEmail, password: loginPassword });
+localStorage.setItem('users', JSON.stringify(users)); 
 
-alert("Sesion iniciada con exito " );
+    // const alertHTML = `
+    //   <div class="alert alert-warning alert-dismissible fade show" role="alert">
+    //     Correo y/o contraseña incorrectos. Por favor, verifica tus datos.
+    //     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    //   </div>
+    // `;
 
-})
+    // container.innerHTML = alertHTML;
+
+});
+
+//Redirigir a incio
+
+function logInForm() {
+    window.location.href = 'index.html'
+}
