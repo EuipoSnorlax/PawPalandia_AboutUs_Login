07e7b8bffd110f31ebeaddc8932affbf2a77da74
Romina <cv6494@innovaccion.mx>
2024-11-17 02:11:45 -0600

    Merge fixed

--- log-in.js
+++ log-in.js
@@ -18,21 +18,4 @@ localStorage.setItem('users', JSON.stringify(users));
 
 alert("Sesion iniciada con exito " );
 
-    e.preventDefault();
-
-    const loginEmail = document.querySelector('#loginEmail').value;
-    const loginPassword = document.querySelector('#loginPassword').value;
-
-    const users = JSON.parse(localStorage.getItem('users')) || [];
-
-    const userRegister = users.find(user => user.email === loginEmail);
-    if (userRegister) {
-        return alert("El usuario ya está registrado");
-    }
-
-    users.push({ email: loginEmail, password: loginPassword });
-    localStorage.setItem('users', JSON.stringify(users));
-
-    alert("Usuario registrado con éxito");
-
 })
