1c1e6ccead6d6335e2414dd5d81a86f9578ae070
Manuel <jmanuel.lmz@gmail.com>
2024-11-24 18:18:58 -0600

    Banners de pagina principal añadidos

--- main.js
+++ main.js
@@ -457,7 +457,24 @@ function themeGlassEffect() {
 }
 themeGlassEffect();
 
+/* ----------------------------------------
+            Switch Cats and Dogs selector
+------------------------------------------- */
+function perros(){
+  document.getElementById("perros").style.display = "block";
+  document.getElementById("gatos").style.display = "none";
 
+  document.getElementById("perrosButton").classList.add("active");
+  document.getElementById("gatosButton").classList.remove("active");
+}
+
+function gatos(){
+  document.getElementById("gatos").style.display = "block";
+  document.getElementById("perros").style.display = "none";
+
+  document.getElementById("gatosButton").classList.add("active");
+  document.getElementById("perrosButton").classList.remove("active");
+}
 
 /* ----------------------------------------
             Product Registration Form
